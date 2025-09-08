edc57c93261832ca550a9508576fd14b86186760
althausLuca <luca.althaus@unibe.ch>
2025-09-08 16:33:57 +0200

    checking for iiif context in json files

---
+++ cypress/tests/files.js
@@ -0,0 +1,22 @@
+export const Files = [
+	{
+		name: 'test.png',
+		path: 'plugins/generic/iiifViewer/cypress/tests/data/IIIF_example.png',
+		type: 'image/png'
+	},
+	{
+		name: 'IIIF_example.json',
+		path: 'plugins/generic/iiifViewer/cypress/tests/data/test.json',
+		type: 'text/json'
+	},
+	{
+		name: 'IIIF_example.json',
+		path: 'plugins/generic/iiifViewer/cypress/tests/data/IIIF_example.json',
+		type: 'text/json'
+	},
+	{
+		name: 'test.jpg',
+		path: 'plugins/generic/iiifViewer/cypress/tests/data/IIIF_example.jpg',
+		type: 'image/png'
+	},
+];
