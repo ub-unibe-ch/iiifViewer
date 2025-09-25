dd224ae340171ea166172573dc6537a2b2ebec69
althausLuca <luca.althaus@unibe.ch>
2025-09-25 09:57:02 +0200

    --no-sandbox removed

--- .github/actions/tests.sh
+++ .github/actions/tests.sh
@@ -1,6 +1,6 @@
 echo "Run cypress tests"
 
 export TERM=xterm
-npx cypress run --no-sandbox --config   '{"specPattern":["plugins/generic/iiifViewer/cypress/tests/functional/*.cy.js"]}'
+npx cypress run --config  '{"specPattern":["plugins/generic/iiifViewer/cypress/tests/functional/*.cy.js"]}'
 
 echo "Validate  against erudit-style"
