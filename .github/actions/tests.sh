a0368fb76d32e430fe83b362b613096c5bb312cc
althausLuca <luca.althaus@unibe.ch>
2025-09-25 12:56:55 +0200

    gpu disabling but enable rasterization  added to cypress

--- .github/actions/tests.sh
+++ .github/actions/tests.sh
@@ -1,5 +1,4 @@
 echo "Run cypress tests"
-npx cypress run --browser electron --headless -- --disable-gpu --disable-software-rasterizer \
-  --config '{"specPattern":["plugins/generic/iiifViewer/cypress/tests/functional/*.cy.js"]}'
+npx cypress run --browser electron --headless -- --disable-gpu --config '{"specPattern":["plugins/generic/iiifViewer/cypress/tests/functional/*.cy.js"]}'
 
 echo "Validate  against erudit-style"
