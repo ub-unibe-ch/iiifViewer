4e17cd169ed685fb93dc97053f921665d1b4713a
althausLuca <luca.althaus@unibe.ch>
2025-09-25 15:29:40 +0200

    some files

--- cypress/tests/files.js
+++ cypress/tests/files.js
@@ -1,19 +1,19 @@
 export const Files = [
-	// {
-	// 	name: 'test.png',
-	// 	path: 'plugins/generic/iiifViewer/cypress/tests/data/IIIF_example.png',
-	// 	type: 'image/png'
-	// },
-	// {
-	// 	name: 'IIIF_example.json',
-	// 	path: 'plugins/generic/iiifViewer/cypress/tests/data/IIIF_example.json',
-	// 	type: 'text/json'
-	// },
-	// {
-	// 	name: 'test.jpg',
-	// 	path: 'plugins/generic/iiifViewer/cypress/tests/data/IIIF_example.jpg',
-	// 	type: 'image/png'
-	// },
+	{
+		name: 'test.png',
+		path: 'plugins/generic/iiifViewer/cypress/tests/data/IIIF_example.png',
+		type: 'image/png'
+	},
+	{
+		name: 'IIIF_example.json',
+		path: 'plugins/generic/iiifViewer/cypress/tests/data/IIIF_example.json',
+		type: 'text/json'
+	},
+	{
+		name: 'test.jpg',
+		path: 'plugins/generic/iiifViewer/cypress/tests/data/IIIF_example.jpg',
+		type: 'image/png'
+	},
 	// {
 	// 	name: 'NotAManifest.json',
 	// 	path: 'plugins/generic/iiifViewer/cypress/tests/data/NotAManifest.json',
