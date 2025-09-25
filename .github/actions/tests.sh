2da9bc0e6b5ff8e1594e7999ba37204bfc721c52
althausLuca <43413156+althausLuca@users.noreply.github.com>
2025-09-25 14:04:30 +0200

    Update tests.sh

--- .github/actions/tests.sh
+++ .github/actions/tests.sh
@@ -1,4 +1,4 @@
 echo "Run cypress tests"
-npx cypress run --headless --disable-gpu --config '{"specPattern":["plugins/generic/iiifViewer/cypress/tests/functional/*.cy.js"]}'
+npx cypress run --headless --config '{"specPattern":["plugins/generic/iiifViewer/cypress/tests/functional/*.cy.js"]}'
 
 echo "Validate  against erudit-style"
