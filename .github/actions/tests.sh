fffedde7948f6cc409b399aeac08a267ae194136
althausLuca <luca.althaus@unibe.ch>
2025-09-25 12:47:33 +0200

    gpu dissabling added to cypress

--- .github/actions/tests.sh
+++ .github/actions/tests.sh
@@ -1,4 +1,5 @@
 echo "Run cypress tests"
-npx cypress run  --headless --config  '{"specPattern":["plugins/generic/iiifViewer/cypress/tests/functional/*.cy.js"]}'
+npx cypress run --browser electron --headless -- --disable-gpu --disable-software-rasterizer \
+  --config '{"specPattern":["plugins/generic/iiifViewer/cypress/tests/functional/*.cy.js"]}'
 
 echo "Validate  against erudit-style"
