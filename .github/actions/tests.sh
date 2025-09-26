b4d0db2ae5934065640ac661d85aa330162aada1
althausLuca <43413156+althausLuca@users.noreply.github.com>
2025-09-26 09:00:21 +0200

    Update tests.sh

--- .github/actions/tests.sh
+++ .github/actions/tests.sh
@@ -5,4 +5,4 @@ set -e
 echo "Run cypress tests"
 npx cypress run  --browser chrome --headed --config  '{"specPattern":["plugins/generic/iiifViewer/cypress/tests/functional/*.cy.js"]}'
 
-echo "Validate  against erudit-style"
+echo "Done"
