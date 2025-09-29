54a22946ef931cd1700e9b1454fb2a8c82eebb5a
althausLuca <43413156+althausLuca@users.noreply.github.com>
2025-09-29 11:55:07 +0200

    Omp 3.4 stable conflixt fix (#54)
    * Resolving test README conflict
    
    * files.js update

--- cypress/tests/files.js
+++ cypress/tests/files.js
@@ -2,16 +2,16 @@ export const Files = [
 	{
 		name: 'test.png',
 		path: 'plugins/generic/iiifViewer/cypress/tests/data/IIIF_example.png',
-		type: 'image/png',
+		type: 'image/png'
 	},
 	{
 		name: 'IIIF_example.json',
 		path: 'plugins/generic/iiifViewer/cypress/tests/data/IIIF_example.json',
-		type: 'text/json',
+		type: 'text/json'
 	},
 	{
 		name: 'test.jpg',
 		path: 'plugins/generic/iiifViewer/cypress/tests/data/IIIF_example.jpg',
-		type: 'image/png',
+		type: 'image/png'
 	},
 ];
